dfa25e5bfbf7f9e99089becf068bd17834c7a03f
UA CR <blazer5@email.arizona.edu>
2021-07-15 22:16:29 -0700

    added port for heroku

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const { notes } = require('./db/db');
@@ -9,6 +10,6 @@ app.get('/api/db', (req, res) => {
     res.json(results);
 })
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
