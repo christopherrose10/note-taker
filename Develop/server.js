45fe6fd8d78db9075fc23344640f51fac53a73d2
UA CR <blazer5@email.arizona.edu>
2021-07-18 16:54:00 -0700

    start script added

--- Develop/server.js
+++ Develop/server.js
@@ -4,19 +4,19 @@ const fs = require('fs');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
 
 // parse incoming CSS and JS data
 app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
-app.use('/html', htmlRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
